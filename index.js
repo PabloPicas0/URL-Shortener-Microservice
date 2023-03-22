491bb03fca6be26f751b107aad25021bc6374a61
PabloPicas0 <pawel.fe@gmail.com>
2023-03-22 23:34:10 +0100

    Added api route

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
-})
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-const port = process.env.PORT || 3000
+app.post("/api/shorturl", (req, res) => {
+  res.json({ message: "panw3d" });
+});
+
+const port = process.env.PORT || 3000;
 const listener = app.listen(port, () => {
-    console.log(`Your app is listening at localhost:${listener.address().port}`);
-    console.log("Press ctrl + c to exit")
-})
+  console.log(`Your app is listening at localhost:${listener.address().port}`);
+  console.log("Press ctrl + c to exit");
+});
